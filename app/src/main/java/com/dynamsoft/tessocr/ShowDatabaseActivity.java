084cff2bc4b23380a8880866f734f664ef8cdea3
tsujibiha <arrowshaft@gmail.com>
2015-12-01 10:26:17 -0500

    added button to main ui
    started to create new activity, showing db contents

---
+++ app/src/main/java/com/dynamsoft/tessocr/ShowDatabaseActivity.java
@@ -0,0 +1,14 @@
+package com.dynamsoft.tessocr;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ShowDatabaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_database);
+    }
+
+}
